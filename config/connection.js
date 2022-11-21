7a915a8261dc119a631f5fcff0ecf89afd27beee
andrew <ammassey42@gmail.com>
2022-11-21 14:52:19 -0800

    started file structure

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+// Require sequelize package
+const Sequelize = require('sequelize');
+// Require and configure dotenv package
+require('dotenv').config();
+
+let sequelize;
+
+// Create new sequelize instance, using our environment variables.
+sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    }
+);
+
+// Export sequelize instance for use in other files.
+module.exports = sequelize;
